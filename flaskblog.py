a3e09d5c192faf0161c46151aabf8594d35ec589
andesdevroot <andes.dev.root@gmail.com>
2020-06-14 23:21:16 -0400

    se crean Rutas de paginas HOME + ABOUT

--- flaskblog.py
+++ flaskblog.py
@@ -3,12 +3,17 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+@app.route('/home')
+def home():
     return '<h1>Home Page</h1>'
+
+
+@app.route('/about')
+def about():
+    return '<h1>About Page</h1>'
 
 
 # script para que siempre la app este en modo debugger
 
 if __name__ == '__main__':
     app.run(debug=True)
-

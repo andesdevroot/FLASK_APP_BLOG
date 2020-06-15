15ae6b1bb127b1f69140e9679d394bf6ce8e314f
andesdevroot <andes.dev.root@gmail.com>
2020-06-14 23:57:57 -0400

    se crea loop del contenido del post en vistas

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,31 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Cesar rivas',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'Abril'
+    },
+    {
+        'author': 'Carlos',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'Mayo'
+    },
+]
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return '<h1>About Page</h1>'
+    return render_template('about.html')
 
 
 # script para que siempre la app este en modo debugger

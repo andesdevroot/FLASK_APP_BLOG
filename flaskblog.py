17f2ccb46b65fa2677649ab422a8b8f6a6be8cdc
andesdevroot <andes.dev.root@gmail.com>
2020-06-16 18:18:22 -0400

    formulario de registro + validaciones OK

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for 
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
@@ -31,11 +31,14 @@ def about():
     return render_template('about.html', title='About')
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Cuenta Creada para {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
-
+    
    
 @app.route('/login')
 def login():

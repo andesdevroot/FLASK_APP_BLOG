0c23b4b4976c47d7df914b90353012a39c4337dd
andesdevroot <andes.dev.root@gmail.com>
2020-06-15 23:31:50 -0400

    se actualizan formularios

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Lenght, Email, EqualTo
 
 
@@ -14,7 +14,18 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
                                       validators=[DataRequired(), EqualTo('password')])
+    Submit = SubmitField('Sign Up')
     
+    
+    
+    class LoginForm(FlaskForm):    
+        email = StringField('Email',
+                            validators=[DataRequired(), Email()])
+        password = PasswordField('Password', validators=[DataRequired()])
+        remember = BooleanField('Remember Me')
+        Submit = SubmitField('Login')
+    
+     
     
     
     

2b237616b180f2fb02646ba69f9461b4139c2392
andesdevroot <andes.dev.root@gmail.com>
2020-06-17 20:12:17 -0400

    se crean modelos y base de datos USER + POSTS

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,36 @@
+from datetime import  datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-app = Flask(__name__)
 
+
+app = Flask(__name__)
 app.config['SECRET_KEY'] = '5a770abe765f4a8ea2b798f4d88b5bfd'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+# modelo Usuario DATABASE
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', {self.image_file}')"
+    
+    
+    
+# modelo Post DATABASE
+class Post(db.Model):
+     id = db.Column(db.Integer, primary_key=True)
+     title = db.Column(db.String(100), nullable=False)
+     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+     content = db.Column(db.Text, nullable=False)
+     
+     
+
 
 posts = [
     {
